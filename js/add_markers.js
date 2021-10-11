9cfb53d1870575acd22d7151a8f33acec87f34a1
Sraav Gamer <sraav.3@gmail.com>
2021-10-11 18:56:12 +0530

    first

--- js/add_markers.js
+++ js/add_markers.js
@@ -14,93 +14,125 @@ AFRAME.registerComponent("create-markers", {
             marker.setAttribute("markerhandler", {});
             mainScene.appendChild(marker);
 
-            var model = document.createElement("a-entity");         
-            model.setAttribute("id", `model-${toy.id}`);
-            model.setAttribute("position", toy.model_geometry.position);
-            model.setAttribute("rotation", toy.model_geometry.rotation);
-            model.setAttribute("scale", toy.model_geometry.scale);
-            model.setAttribute("gltf-model", `url(${toy.model_url})`);
-            model.setAttribute("gesture-handler", {});
-            model.setAttribute("animation-mixer", {});
-            marker.appendChild(model);
+            if(toy.is_out_of_stock == true){
+              // model
+              var model = document.createElement("a-entity");         
+              model.setAttribute("id", `model-${toy.id}`);
+              model.setAttribute("position", toy.model_geometry.position);
+              model.setAttribute("rotation", toy.model_geometry.rotation);
+              model.setAttribute("scale", toy.model_geometry.scale);
+              model.setAttribute("gltf-model", `url(${toy.model_url})`);
+              model.setAttribute("gesture-handler", {});
+              model.setAttribute("animation-mixer", {});
+              model.setAttribute("visible", false);
+              marker.appendChild(model);
 
-            // description Containerge
-            var mainPlane = document.createElement("a-plane");
-            mainPlane.setAttribute("id", `main-plane-${toy.id}`);
-            mainPlane.setAttribute("position", { x: 0, y: 0, z: 0 });
-            mainPlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
-            mainPlane.setAttribute("width", 1.5);
-            mainPlane.setAttribute("height", 1.5);
-            mainPlane.setAttribute("material", { color: "#FDFF98" });
-            marker.appendChild(mainPlane);
+              // description Containerge
+              var mainPlane = document.createElement("a-plane");
+              mainPlane.setAttribute("id", `main-plane-${toy.id}`);
+              mainPlane.setAttribute("position", { x: 0, y: 0, z: 0 });
+              mainPlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
+              mainPlane.setAttribute("width", 1.5);
+              mainPlane.setAttribute("height", 1.5);
+              mainPlane.setAttribute("material", { color: "#FDFF98" });
+              mainPlane.setAttribute("visible", false);
+              marker.appendChild(mainPlane);
 
-            // toy title background plane
-            var titlePlane = document.createElement("a-plane");
-            titlePlane.setAttribute("id", `title-plane-${toy.id}`);
-            titlePlane.setAttribute("position", { x: 0, y: 0.89, z: 0.02 });
-            titlePlane.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-            titlePlane.setAttribute("width", 1.49);
-            titlePlane.setAttribute("height", 0.3);
-            titlePlane.setAttribute("material", { color: "#DC3546" });
-            mainPlane.appendChild(titlePlane);
+              // toy title background plane
+              var titlePlane = document.createElement("a-plane");
+              titlePlane.setAttribute("id", `title-plane-${toy.id}`);
+              titlePlane.setAttribute("position", { x: 0, y: 0.89, z: 0.02 });
+              titlePlane.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+              titlePlane.setAttribute("width", 1.49);
+              titlePlane.setAttribute("height", 0.3);
+              titlePlane.setAttribute("material", { color: "#DC3546" });
+              mainPlane.appendChild(titlePlane);
 
-            // toy title
-            var toyTitle = document.createElement("a-entity");
-            toyTitle.setAttribute("id", `toy-title-${toy.id}`);
-            toyTitle.setAttribute("position", { x: 0, y: 0, z: 0.1 });
-            toyTitle.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-            toyTitle.setAttribute("text", {
-              font: "monoid",
-              color: "black",
-              width: 2.4,
-              height: 1,
-              align: "center",
-              value: toy.toy.toUpperCase()
-            });
-            titlePlane.appendChild(toyTitle);
+              // toy title
+              var toyTitle = document.createElement("a-entity");
+              toyTitle.setAttribute("id", `toy-title-${toy.id}`);
+              toyTitle.setAttribute("position", { x: 0, y: 0, z: 0.1 });
+              toyTitle.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+              toyTitle.setAttribute("text", {
+                font: "monoid",
+                color: "black",
+                width: 2.4,
+                height: 1,
+                align: "center",
+                value: toy.toy.toUpperCase()
+              });
+              titlePlane.appendChild(toyTitle);
 
-            // Description
-            var description = document.createElement("a-entity");
-            description.setAttribute("id", `description-${toy.id}`);
-            description.setAttribute("position", { x: 0, y: 0.25, z: 0.1 });
-            description.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-            description.setAttribute("text", {
-              font: "monoid",
-              color: "brown",
-              width: 1.4,
-              height: 0.5,
-              align: "center",
-              value: toy.description
-            });
-            mainPlane.appendChild(description);
+              // Description
+              var description = document.createElement("a-entity");
+              description.setAttribute("id", `description-${toy.id}`);
+              description.setAttribute("position", { x: 0, y: 0.25, z: 0.1 });
+              description.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+              description.setAttribute("text", {
+                font: "monoid",
+                color: "brown",
+                width: 1.4,
+                height: 0.5,
+                align: "center",
+                value: toy.description
+              });
+              mainPlane.appendChild(description);
 
-            var comesWith = document.createElement("a-entity");
-            comesWith.setAttribute("id", `comes_with-${toy.id}`);
-            comesWith.setAttribute("position", { x: 0, y: -0.25, z: 0.1 });
-            comesWith.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-            comesWith.setAttribute("text", {
-              font: "monoid",
-              color: "brown",
-              width: 1.4,
-              height: 0.5,
-              align: "center",
-              value: toy.comes_with
-            });
-            mainPlane.appendChild(comesWith);
+              // The things the toy comes with
+              var comesWith = document.createElement("a-entity");
+              comesWith.setAttribute("id", `comes-with-${toy.id}`);
+              comesWith.setAttribute("position", { x: 0, y: -0.25, z: 0.1 });
+              comesWith.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+              comesWith.setAttribute("text", {
+                font: "monoid",
+                color: "brown",
+                width: 1.4,
+                height: 0.5,
+                align: "center",
+                value: toy.comes_with
+              });
+              mainPlane.appendChild(comesWith);
 
-            var age_group = document.createElement("a-entity");
-            age_group.setAttribute("id", `age_group-${toy.id}`);
-            age_group.setAttribute("position", { x: 0, y: -0.5, z: 0.1 });
-            age_group.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-            age_group.setAttribute("text", {
-              font: "monoid",
-              color: "brown",
-              width: 1.4,
-              height: 0.5,
-              align: "center",
-              value: toy.age_group
-            });
-            mainPlane.appendChild(age_group);
+              // Age group
+              var age_group = document.createElement("a-entity");
+              age_group.setAttribute("id", `age-group-${toy.id}`);
+              age_group.setAttribute("position", { x: 0, y: -0.5, z: 0.1 });
+              age_group.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+              age_group.setAttribute("text", {
+                font: "monoid",
+                color: "brown",
+                width: 1.4,
+                height: 0.5,
+                align: "center",
+                value: toy.age_group
+              });
+              mainPlane.appendChild(age_group);
+
+              // Price Plane
+              var pricePlane = document.createElement("a-image");
+              pricePlane.setAttribute("id", `price-plane-${toy.id}`);
+              pricePlane.setAttribute("src","https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/black-circle.png")
+              pricePlane.setAttribute("width",0.8);
+              pricePlane.setAttribute("height",0.8);
+              pricePlane.setAttribute("position",{x:-1.3,y:0,z:0.3});
+              pricePlane.setAttribute("rotation",{x:-90,y:0,z:0});
+              pricePlane.setAttribute("visible", false);
+
+              // Price of the toy
+              var price = document.createElement("a-entity");
+              price.setAttribute("id", `price-${toy.id}`);
+              price.setAttribute("position", {x:0.05, y:0.05, z:0.1});
+              price.setAttribute("rotation", {x:0, y:0, z:0});
+              price.setAttribute("text",{
+                font: "mozillavr",
+                color: "white",
+                width: 2,
+                align: "center",
+                value: `Only ${toy.price}/-`
+              });
+              pricePlane.appendChild(price);
+              marker.appendChild(pricePlane);
+            }
         })
     },
 
--- js/markerhandler.js
+++ js/markerhandler.js
@@ -1,36 +1,128 @@
+var userId = null;
 AFRAME.registerComponent("markerhandler",{
     init: async function(){
+        if(userId === null){
+          this.askUserId();
+        }
+        var toys = await this.getToys();
         this.el.addEventListener("markerFound", ()=>{
-            console.log("marker found");
-            this.handleMarkerFound();
+          if (userId !== null){
+            var markerId = this.el.id;
+            this.handleMarkerFound(toys, markerId);
+          }
         });
         this.el.addEventListener("markerLost", ()=>{
-            console.log("marker lost");
             this.handleMarkerLost();
         })
     },
-    handleMarkerFound: function(){
-        var buttondiv = document.getElementById("button-div");
-        buttondiv.style.display = "flex"
-        var summaryButton = document.getElementById("order-summary-button");
-        var orderButton = document.getElementById("order-button");
-        summaryButton.addEventListener("click",function(){
-            swal({
-                icon:"warning",
-                title:"Order Summary",
-                text:"Work in progress"
-            })
+
+    askUserId: function(){
+        swal({
+          title: "Welcome to Toy Shop!",
+          icon: "https://us.123rf.com/450wm/svetlam87/svetlam871810/svetlam87181000048/114979737-toys-store-flat-vector-illustration.jpg?ver=6",
+          content:{
+            element: "input",
+            attributes: {
+              placeholder: "Type your uid Ex:( 1 or 2 or 21 )",
+              type:"number",
+              min: 1
+            }
+          },
+          closeOnClickOutside: false,
+        }).then(inputValue =>{
+          userId = inputValue;
         })
-        orderButton.addEventListener("click",function(){
+    },
+    handleMarkerFound: function(toys, markerId){
+        var toy = toys.filter(toy => toy.id === markerId[0]);
+        if(toy.is_out_of_stock === true){
             swal({
-                icon:"https://i.imgur.com/4NZ6uLY.jpg",
-                title:"Thanks for your order!!",
-                text:"your order will be there with you soon"
+                icon: "warning",
+                title: toy.toy.toUpperCase(),
+                text: "This toy is out of stock. So Sorry!!",
+                timer: 3500,
+                buttons:false,
             })
-        })
+        }
+        else{
+            var model = document.getElementById(`model-${toy.id}`);
+            model.setAttribute("visible", true);
+
+            var mainPlane = document.getElementById(`main-plane-${toy.id}`);
+            mainPlane.setAttribute("visible", true);
+
+            var priceplane = document.getElementById(`price-plane-${toy.id}`);
+            priceplane.setAttribute("visible", true)
+
+            // Changing button div visibility
+            var buttonDiv = document.getElementById("button-div");
+            buttonDiv.style.display = "flex"; 
+            var summaryButton = document.getElementById("order-summary-button");
+            var orderButtton = document.getElementById("order-button");
+            summaryButton.addEventListener("click", function () {
+              swal({
+                icon: "warning",
+                title: "Order Summary",
+                text: "Work In Progress"
+              });
+            });
+        
+            orderButtton.addEventListener("click", () => {
+              var uID;
+              userId <=9 ? (uID = `U0${userId}`) : (uID = `U${userId}`);
+              this.handleOrder(uID, toy)
+              swal({
+                icon: "https://i.imgur.com/4NZ6uLY.jpg",
+                title: "Thanks For Order !",
+                text: "Your order will be there with you soon!",
+                timer: 3500,
+                buttons: false
+              });
+            });
+        }
     },
     handleMarkerLost: function(){
         var buttondiv = document.getElementById("button-div");
         buttondiv.style.display = "none"
-    }
+    },
+
+    handleOrder: function(uid, toy){
+      firebase.firestore()
+      .collection("users")
+      .doc(uid)
+      .get()
+      .then(doc => {
+        var details = doc.data();
+        
+        if(details["current_orders"][toy.id]){
+          details["current_orders"][toy.id]["quantity"] += 1;
+          var currentQuantity = details["current_orders"][toy.id]["quantity"];
+          details["current_orders"][toy.id]["subtotal"] = currentQuantity * toy.price;
+        }else{
+          details["current_orders"][toy.id] = {
+            item: toy.toy,
+            price: toy.price,
+            quantity: 1,
+            subtotal: currentQuantity * 1
+          };
+        }
+
+        details.total_bill += toy.price
+
+        firebase.firestore()
+        .collection("users")
+        .doc(doc.id)
+        .update(details)
+      })
+    },  
+
+    getToys: async function () {
+        return await firebase
+          .firestore()
+          .collection("toys")
+          .get()
+          .then(snap => {
+            return snap.docs.map(doc => doc.data());
+          });
+      },
 })
